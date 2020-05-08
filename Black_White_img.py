3a7976e538d752070abf137655b6d3fe9d42f0eb
Shrouk Ragheb Amer <36502437+ShroukRagheb@users.noreply.github.com>
2020-05-09 01:44:37 +0200

    Update Black_White_img.py

--- Black_White_img.py
+++ Black_White_img.py
@@ -25,3 +25,24 @@ net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, n
 def color(frame): 
   # scale the pixel intensities to the range [0, 1]
   img_rgb = ( frame * 1.0 / 255).astype(np.float32
+  #convert to Lab color space
+  img_lab = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2Lab)
+   
+  # pull out L channel
+  img_l = img_lab[:,:,0]
+
+  # original image size
+  (H_orig,W_orig) = img_rgb.shape[:2] 
+
+  # resize image to network input size
+  img_rs = cv2.resize(img_rgb, (W_in, H_in))
+  img_lab_rs = cv2.cvtColor(img_rs, cv2.COLOR_BGR2Lab)
+  img_l_rs = img_lab_rs[:,:,0]
+
+  # subtract 50 for mean-centering
+  img_l_rs -= 50 
+
+  # pass the L channel through the network which will predict the 'a' and 'b' channel values
+  net.setInput(cv2.dnn.blobFromImage(img_l_rs))
+
+  #Inference on network                                      
