b8fcbee9362f25e41ee2e7c5201d65d6ef52cba6
AbdullahGalalMohamed <36282511+AbdullahGalalMohamed@users.noreply.github.com>
2020-05-09 01:47:34 +0200

    Create Black_White_video.py

---
+++ Black_White_video.py
@@ -0,0 +1,48 @@
+import cv2
+import shutil
+from moviepy.editor import VideoFileClip, AudioFileClip
+import moviepy.editor as mpe
+import os
+import numpy as np
+from PIL import Image
+import glob
+import errno, stat
+import time
+import sys
+input = sys.argv[1]
+#Network input size
+W_in = 224
+H_in = 224
+# Create network graph and load weights
+net = cv2.dnn.readNetFromCaffe('C:/Users/abdullah/Desktop/IP_project/colorization_deploy_v2.prototxt', 'C:/Users/abdullah/Desktop/IP_project/colorization_release_v2.caffemodel')
+# load cluster centers
+pts_in_hull = np.load('C:/Users/abdullah/Desktop/IP_project/pts_in_hull.npy') 
+# populate cluster centers as 1x1 convolution kernel
+pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1)
+net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull.astype(np.float32)]
+net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, np.float32)]
+def handleRemoveReadonly(func, path, exc):
+  excvalue = exc[1]
+  if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+      os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+      func(path)
+  else:
+      raise
+def color(frame): 
+    # scale the pixel intensities to the range [0, 1]
+    img_rgb = ( frame * 1.0 / 255).astype(np.float32)
+    #convert to Lab color space
+    img_lab = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2Lab)
+    # pull out L channel
+    img_l = img_lab[:,:,0]
+    # original image size
+    (H_orig,W_orig) = img_rgb.shape[:2] 
+    # resize image to network input size
+    img_rs = cv2.resize(img_rgb, (W_in, H_in))
+    img_lab_rs = cv2.cvtColor(img_rs, cv2.COLOR_BGR2Lab)
+    img_l_rs = img_lab_rs[:,:,0]
+    # subtract 50 for mean-centering
+    img_l_rs -= 50 
+    # pass the L channel through the network which will predict the 'a' and 'b' channel values
+    net.setInput(cv2.dnn.blobFromImage(img_l_rs))
+    #Inference on network
