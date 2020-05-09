f9dc971c2ee572cd09305bbf00db4c3984df385a
Shrouk Ragheb Amer <36502437+ShroukRagheb@users.noreply.github.com>
2020-05-09 02:24:35 +0200

    Update p1.php

--- p1.php
+++ p1.php
@@ -296,3 +296,292 @@ label{
 
 <div class="show" id="0" style="display:block; width:80.5%; height:96%; background-color:gray; position: absolute;
   left:18.7%; top:2%; bottom:2%;">
+   <div class="V3"></div>
+  <div style=" height:50% ;position:absolute; top:2%; left:2%;">
+  <img id="I01" src="baby1.jpg" style="width:28%; height:98%;"/>
+</div>
+<div style=" height:50% ;position:absolute; top:2%; left:26%;">
+  <img id="I011" src="baby2.jpg" style="width:28%; height:98%;"/>
+</div>
+<div class="split left" style="display:block;  width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:5%; top:68%; bottom:0;"></br></br>
+    <p1>Select Your Photo then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file01" class="btn cartoonize">Select Your Photo</label>
+    <input id="file01" name="file01" onchange="readURL0(this);"  type="file"><br><br>
+  <!--<input type="file" name="file11" id="file11" onchange="readURL1(this);"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit01"/>
+</div>
+</br>
+</form>
+</div></br>    
+  <!--<input type="file" name="file01" id="file01" onchange="readURL0(this);"/>-->
+
+<div style="display:block; width:6.5%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:22.7%; top:82%; bottom:0;"></div>
+<div style="display:block; width:49.5%; height:98.3%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:50.2%; top:0%; bottom:0;">
+<video id="player0" alt="your video" src="AMR.mp4" width="300" height="400" controls></video>
+<video id="player02" alt="your video"   src="AMR2.mp4" width="300" height="400" controls></video></br>
+</div>
+<div style="display:block; width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:55%; top:68%; bottom:0;"></br></br>
+  <p1>Select Your Video then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file02" class="btn cartoonize">Select Your Video</label>
+    <input id="file02" type="file" name="file02" accept="video/*" capture="camera" id="recorder0"/><br><br>
+  <!--<input type="file" name="file12" id="file12" accept="video/*" capture="camera" id="recorder1"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit02"/>
+</div>
+</div>
+<div style="display:block; width:6.7%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:72.5%; top:82.5%; bottom:0;">
+    </div>
+</div>
+  
+  
+<div class="show" id="1"  style="display:block; width:80.5%; height:96%; background-color:lightgray; position: absolute;
+  left:18.7%; top:2%; bottom:2%;">
+  <div class="V3"></div>
+  
+    <div style=" height:50% ;position:absolute; top:2%; left:2%;">
+    <img id="I11" src="bg1.jpg" style="width:44%; height:98%;"/>
+  </div>
+  <div style=" height:50% ;position:absolute; top:2%; left:25%;">
+  <img id="I111" src="B&W.jpg" style="width:44%; height:98%;"/>
+</div>
+<div class="split left" style="display:block;  width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:gray; position:absolute;
+    left:5%; top:68%; bottom:0;"></br></br>
+  <p1>Select Your Photo then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file11" class="btn cartoonize">Select Your Photo</label>
+    <input id="file11" type="file" name="file11" onchange="readURL1(this);"/><br><br>
+  <!--<input type="file" name="file11" id="file11" onchange="readURL1(this);"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit11"/>
+</div>
+</br>
+</form>
+</div></br>
+<div style="display:block; width:6.5%; height:3%; padding: 0.5% 0.5% ;background-color:gray; position:absolute;
+    left:22.7%; top:82%; bottom:0;"></div>
+<div style="display:block; width:49.5%; height:98.3%; padding: 0.5% 0.5%; background-color:lightgray; position:absolute;
+    left:50.2%; top:0%; bottom:0;">
+<video id="player1" alt="your video" src="corona.mp4" width="300" height="400" controls></video>
+<video id="player12" alt="your video"   src="corona2.mp4" width="300" height="400" controls></video></br>
+</div>
+<div style="display:block; width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:gray; position:absolute;
+    left:55%; top:68%; bottom:0;"></br></br>
+  <p1>Select Your Video then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file12" class="btn cartoonize">Select Your Video</label>
+    <input id="file12" type="file" name="file12" accept="video/*" capture="camera" id="recorder1"/><br><br>
+  <!--<input type="file" name="file12" id="file12" accept="video/*" capture="camera" id="recorder1"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit12"/>
+</div>
+</form>
+</div>
+<div style="display:block; width:6.7%; height:3%; padding: 0.5% 0.5% ;background-color:gray; position:absolute;
+    left:72.5%; top:82.5%; bottom:0;">
+</div>
+</div>
+  
+  
+<div class="show" id="2" style="display:block; width:80.5%; height:96%; background-color:gray; position: absolute;
+  left:18.7%; top:2%; bottom:2%;">
+  <div class="V3"></div>
+ <div style=" height:50% ;position:absolute; top:2%; left:2%;">
+  <img id="I21" src="black.jpg" style="width:98%; height:98%;"/>
+</div>
+<div style=" height:50% ;position:absolute; top:2%; left:25%;">
+  <img id="I22" src="black2.jpg" style="width:98%; height:98%;"/>
+</div>
+<div class="split left" style="display:block;  width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:5%; top:68%; bottom:0;"></br></br>
+  <p1 style="color:DarkSlateGrey;">Select Your Photo then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file21" class="btn cartoonize">Select Your Photo</label>
+    <input type="file" name="file21" id="file21" onchange="readURL2(this);"/><br><br>
+  <!--<input type="file" name="file21" id="file21" onchange="readURL2(this);"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit21"/>
+</div>
+    </form>
+</div>
+<div style="display:block; width:6.5%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:22.7%; top:82%; bottom:0;"></div>
+<div style="display:block; width:49.5%; height:98.3%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:50.2%; top:0%; bottom:0;">
+<video id="player2" alt="your video" src="blackvideo.mp4" width="300" height="400" controls></video>
+<video id="player22" alt="your video" src="blackvideo2.mp4" width="300" height="400" controls></video></br>
+</div>
+<div style="display:block; width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:55%; top:68%; bottom:0;"></br></br>
+  <p1 style="color:DarkSlateGrey;">Select Your Video then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file22" class="btn cartoonize">Select Your Video</label>
+    <input id="file22" type="file" name="file22" accept="video/*" capture="camera" id="recorder2"/><br><br>
+  <!--<input type="file" name="file22" id="file22" accept="video/*" capture="camera" id="recorder2"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit22"/>
+</div>
+</div>
+<div style="display:block; width:6.7%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:72.5%; top:82.5%; bottom:0;">
+</div>
+</div>
+  
+  
+<div class="show" id="3" style="display:block; width:80%; height:96%; background-color:lightgray; position: absolute;
+  left:19.2%; top:2%; bottom:2%;">
+  <div style="display:block; width:100%; height:100%; background-color:none; position: absolute;
+  left:13%; top:2%; bottom:10%;"> 
+  <img id="I31" src="gray.jpg"style="width:25%; height:50%;" />
+</div>
+<div style=" width:100%; height:100%;position:absolute; top:2%; left:40%;">
+  <img id="I32" src="back.jpg"style="width:25%; height:50%;" />
+</div>
+<div style=" width:100%; height:100%; position:absolute; top:2%; left:67%;">
+  <img id="I33" src="chbg.jpg"style="width:25%; height:50%;" />
+  </div>
+  <div class="split right" style="display:block; width:66%; height:33%; padding: 0.5% 0.5%;border-radius:10.9% ; background-color:gray; position:absolute;
+    left:18%; top:60%; bottom:0;"></br></br>
+  <p1 style="color:white;">Select Your Photo and New Background then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <div style=" position:absolute; left:5%; top:40%; bottom:0;">
+  <label for="file31" class="btn cartoonize">Select Your Photo</label>
+  <label for="file32" class="btn cartoonize">Select New Background</label>
+ 
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit31"/>
+</div><br><br><br>
+    <input id="file31" type="file" name="file31" onchange="readURL3(this);" /> 
+  <!--<input type="file" name="file31" id="file31" value="image" onchange="readURL3(this);"/>-->
+    <input id="file32" type="file" name="file32" onchange="readURL33(this);"/>
+  <!--<input type="file" name="file32" id="file32" value="New_Background" onchange="readURL33(this);"/>-->
+
+    </form>
+</div>
+<div style="display:block; width:6.7%; height:5%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:18%; top:81.5%; bottom:0;"></div>
+<div style="display:block; width:6.5%; height:5%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:39.55%; top:81.5%; bottom:0;"></div>
+</div>
+</div>
+  
+  
+<div  class="show" id="4" style="display:block; width:80.5%; height:96%; background-color:gray; position: absolute;
+  left:18.7%; top:2%; bottom:2%;">
+  <div class="V3"></div>
+ <div style=" height:50% ;position:absolute; top:2%; left:2%;">
+  <img id="I41" src="gray5.jpg" style="width:38%; height:98%;"/>
+</div>
+<div style=" height:50% ;position:absolute; top:2%; left:25%;">
+  <img id="I42" src="graybackk.jpg" style="width:38%; height:98%;"/>
+</div>
+<div class="split left" style="display:block;  width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:5%; top:68%; bottom:0;"></br></br>
+  <p1 style="color:DarkSlateGrey;">Select Your Photo then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file41" class="btn cartoonize">Select Your Photo</label>
+  <input id="file41" type="file" name="file41" onchange="readURL4(this);"/><br><br>
+  <!--<input type="file" name="file41" id="file41" onchange="readURL4(this);"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit41"/>
+</div>
+    </form>
+</div>
+<div style="display:block; width:6.5%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:22.7%; top:82%; bottom:0;"></div>
+<div style="display:block; width:49.5%; height:98.3%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:50.2%; top:0%; bottom:0;">
+<video id="player4" alt="your video" src="graybackvideo.mp4" width="300" height="400" controls></video>
+<video id="player" alt="your video" src="graybackvideo2.mp4" width="300" height="400" controls></video></br>
+</div>
+<div style="display:block; width:40%; height:29%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:lightgray; position:absolute;
+    left:55%; top:68%; bottom:0;"></br></br>
+  <p1 style="color:DarkSlateGrey;">Select Your Video then click on <ins>CONVERT</ins> button</p1><hr><br>
+  <form action="upload.php" enctype="multipart/form-data" method="post" >
+  <label for="file42" class="btn cartoonize">Select Your Video</label>
+    <input id="file42" type="file" name="file42" accept="video/*" capture="camera" id="recorder4"/><br><br> 
+  <!--<input type="file" name="file42" id="file42" accept="video/*" capture="camera" id="recorder4"/>-->
+  <div style=" position:absolute; left:35%; top:68%; bottom:0;">
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit42"/>
+</div>
+</div>
+<div style="display:block; width:6.7%; height:3%; padding: 0.5% 0.5% ;background-color:lightgray; position:absolute;
+    left:72.5%; top:82.5%; bottom:0;">
+</div>
+</div>
+  
+  
+  <div class="show"  id="5" style="display:block; width:80%; height:96%; background-color:lightgray; position: absolute;
+  left:19.2%; top:2%; bottom:2%;">
+  <div style="display:block; width:66%; height:90%; background-color:none; position: absolute;
+  left:25%; top:2%; bottom:10%;"> 
+  <img id="I51" src="color1.jpg"style="width:40%; height:55%;" />
+</div>
+<div style=" width:66%; height:90%;position:absolute; top:2%; left:53%;">
+  <img id="I52" src="color2.jpg"style="width:40%; height:55%;" />
+  </div>
+  <div class="split right" style="display:block; width:66%; height:37%; padding: 0.5% 0.5%;border-radius: 10.9%; background-color:gray; position:absolute;
+    left:17%; top:60%; bottom:0;"></br></br>
+  <p1 style="color:white;">Select Your Photo and the color range of an object that you want to change, and the New Color, then click on <ins>CONVERT</ins> button</p1><hr>
+  <form action="upload.php" enctype="multipart/form-data" method="post" ><br>
+  <label for="file51" class="btn cartoonize">Select Your Photo</label>
+  <label class="btn cartoonize">Enter first color</label> <input type="color" name="MyColorPicker1" id="MyColorPicker1"/>
+  <label class="btn cartoonize">Enter second color</label> <input type="color" name="MyColorPicker2" id="MyColorPicker2"/><br><br><hr>
+  <label class="btn cartoonize">change to</label>  <input type="color" name="MyColorPicker3" id="MyColorPicker3"/>
+    <input type="file" name="file51" id="file51" value="image" onchange="readURL5(this);"/>
+  <!--<input type="file" name="file51" id="file51" value="image" onchange="readURL5(this);"/><hr/> -->
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit51"/>
+    </form>
+</div>
+<div style="display:block; width:20.55%; height:4%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:32.7%; top:88.5%; bottom:0;"></div>
+  </div>
+  
+
+  <div class="show" id="6" style="display:block; width:80%; height:96%; background-color:lightgray; position: absolute;
+  left:19.2%; top:2%; bottom:2%;">
+  <div style="display:block;  width:100%; height:100%; background-color:none; position: absolute;
+  left:13%; top:2%; bottom:10%;"> 
+  <img id="I61" src="shirt1.jpg"style="width:25%; height:50%;" />
+</div>
+<div style="  width:100%; height:100%;position:absolute; top:2%; left:40%;">
+  <img id="I62" src="shirt2.jpg"style="width:25%; height:50%;" />
+</div>
+<div style="  width:100%; height:100%; position:absolute; top:2%; left:67%;">
+  <img id="I63" src="shirt3.jpg"style="width:25%; height:50%;" />
+  </div>
+  <div class="split right" style="display:block; width:66%; height:33%; padding: 0.5% 0.5%;border-radius:10.9% ; background-color:gray; position:absolute;
+    left:18%; top:60%; bottom:0;"></br></br>
+  <p1 style="color:DarkSlateGrey;">Select Your Photo then click on <ins>CONVERT</ins> button</p1><hr>
+  <form action="upload.php" enctype="multipart/form-data" method="post" ><br>
+  <div style=" position:absolute; left:5%; top:40%; bottom:0;">
+  <label for="file61" class="btn cartoonize">Select Your Photo</label>
+  <label for="file62" class="btn cartoonize">Select New Piece</label>
+  <input type="submit" class="btn cartoonize" value="CONVERT" name="Submit61"/>
+  </div><br><br><br>
+  <input type="file" name="file61" id="file61" onchange="readURL6(this);"/>
+  <!--<input type="file" name="file61" id="file61" value="image" onchange="readURL6(this);"/>-->
+  
+    <input type="file" name="file62" id="file62" onchange="readURL66(this);"/>
+  <!--<input type="file" name="file62" id="file62" value="New_Background" onchange="readURL66(this);"/>-->
+  
+    </form>
+</div>
+<div style="display:block; width:6.7%; height:5%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:18%; top:81.5%; bottom:0;"></div>
+<div style="display:block; width:6.5%; height:5%; padding: 0.5% 0.5%; background-color:gray; position:absolute;
+    left:39.55%; top:81.5%; bottom:0;"></div>
+  </div>
+  
+  <div class="show" id="7" style="display:block; width:85%; height:96%; background-color:RosyBrown; position: absolute;
+  left:18.7%; top:2%; bottom:2%;">
+  <p2><strong>Piece Of Cake</strong></p2>
+  <p3><strong>Get Ready To</strong></p3>
+  <p4><strong>Have Fun!</strong></p4>
+  <img src="best.gif" style=" width: ; height :95%; position:absolute; left:55%"/>
